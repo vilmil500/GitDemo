78539a742bb769455899a6b8e8858769f302a3d9
g1tesh <gitesh.patil0107@gmail.com>
2022-09-21 19:51:00 +0530

    Commiting Gitesh_Branch

--- src/day21/TestGitClass.java
+++ src/day21/TestGitClass.java
@@ -2,6 +2,11 @@ package day21;
 
 public class TestGitClass {
 
+	
+	public void Gitesh_MEthod()
+	{
+		System.out.println("I have added this");
+	}
 	public static void main(String[] args) {
 		System.out.println("Git by Gitesh");
 
