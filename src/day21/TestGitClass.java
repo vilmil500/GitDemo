8e7d38d6907a151a2fce91c6aaaa174a03a6ed61
g1tesh <gitesh.patil0107@gmail.com>
2022-09-21 20:23:16 +0530

    Added new check in code

--- src/day21/TestGitClass.java
+++ src/day21/TestGitClass.java
@@ -6,6 +6,8 @@ public class TestGitClass {
 	public void Gitesh_MEthod()
 	{
 		System.out.println("I have added this");
+		
+		System.out.println("Added additional check");
 	}
 	public static void main(String[] args) {
 		System.out.println("Git by Gitesh");
